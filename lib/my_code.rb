3b0bb2126c4b4c7863258d7fd420cddff6d7a713
José Romero <jdromerov88@gmail.com>
2019-10-26 04:51:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,6 @@
 # Your Code Here
+def map_this(element1, element2)
+  yiel("A #{element1} and #{element2} sandwich")
+end
+
+map_this("hola tengo hambre", "quiero comer un burrito") {|variable| "#{b} con pan"}
